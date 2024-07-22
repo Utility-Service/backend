504755335bc5c76bb9cefdf536eddc5ae7b7bfea
namratajjagtap <jagtap.namu2020@gmail.com>
2024-07-22 14:25:00 +0530

    added query param

--- urban_services_backend/src/main/java/com/backend1/us_backend/controller/UtilityController.java
+++ urban_services_backend/src/main/java/com/backend1/us_backend/controller/UtilityController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.backend1.us_backend.models.UtilityDetails;
 import com.backend1.us_backend.entity.Utility;
 import com.backend1.us_backend.service.UtilityService;
 
@@ -19,10 +19,19 @@ public class UtilityController {
     private UtilityService utilityService;
 
     @CrossOrigin(origins = "http://localhost:3000")
+    // @GetMapping("/utilities")
+    // public List<Utility> getUtilities(){    //api name or controller name
+
+    //     return utilityService.getAllUtilities();   //api calls the method/function from service
+    // }
+
+
     @GetMapping("/utilities")
-    public List<Utility> getUtilities(){    //api name or controller name
+    public List<Utility> getUtilitiesByType(@RequestParam String typeofservice) {
+        if(typeofservice==null)
+        return utilityService.getAllUtilities(); 
+        else
+        return utilityService.getutilitiesByType(typeofservice);
 
-        return utilityService.getAllUtilities();   //api calls the method/function from service
-    }
-
+}
 }
--- urban_services_backend/src/main/java/com/backend1/us_backend/service/UtilityService.java
+++ urban_services_backend/src/main/java/com/backend1/us_backend/service/UtilityService.java
@@ -1,13 +1,11 @@
 package com.backend1.us_backend.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.backend1.us_backend.entity.Utility;
-import com.backend1.us_backend.models.UtilityDetails;
 import com.backend1.us_backend.repository.UtilityRepository;
 
 @Service
@@ -20,5 +18,9 @@ public class UtilityService {
 
         List<Utility> allServices = utilityRepository.findAll();    //list/arr data store all rows
         return allServices;
+    }
+
+    public List<Utility> getutilitiesByType(String typeofservice) {
+        return utilityRepository.findByType(typeofservice);
     }
 }
